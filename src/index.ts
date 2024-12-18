f29acb51d62f4922c5e39655e9c872ed0193d3d6
Johnathan Sanders <josanders@microsoft.com>
2024-12-18 16:43:47 -0600

    index updates

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,42 @@
 import { OPCClient } from './feature/opcua-client/opcua-client.service';
-import { OPCClientBasic } from './feature/opcua-client/opcua-client-basic.service';
-import { DataType } from 'node-opcua';
 import { Environment } from './environment';
 
-const endpointUrl: string = Environment.opcuaServer.endpointUri;
-const namespace: string = Environment.opcuaServer.namespace;
 const nodeId: string = 's=ChevronLD.CommDrivers.RAEtherNet_IPDriver1.RAEtherNet_IPStation1.Tags.Controller Tags.LS01_ManPosnHMI';
 const nodeId1: string = 's=ChevronLD.CommDrivers.RAEtherNet_IPDriver1.RAEtherNet_IPStation1.Tags.Controller Tags.aLeakDetectedZone1';
 const nodeId3: string = 's=ChevronLD.CommDrivers.RAEtherNet_IPDriver1.RAEtherNet_IPStation1.Tags.Controller Tags.BS01_Bypass_SolVlv_Output';
 const nodeId2: string = 's=ChevronLD.Model.Variable1';
-//aLeakDetectedZone1
 
 const main = async (): Promise<void> => {
     return new Promise(async (resolve, reject) => {
         try {
-            console.log('creating client');
-            const opcuaClient: OPCClient = new OPCClient(Environment.opcuaServer.endpointUri, namespace);
+            console.log('Creating Client...');
+            const opcuaClient: OPCClient = new OPCClient(Environment.opcuaServer.endpointUri, Environment.opcuaServer.namespace);
             await opcuaClient.ready;
-            console.log('client created');
+            console.log('Client Created');
             opcuaClient.subscription$.subscribe((message: any) => {
                 switch(message['action']) {
                     case 'changed': 
-                        console.log('got dataValue of', message['value']);
+                        console.log('Value Changed', message['value']);
                         break;
                     case 'created': 
-                        console.log('subscription created');
+                        console.log('Subscription Created');
                         break;
                     case 'keepalive': 
-                        console.log('subscription keepalive');
+                        console.log('Subscription Keepalive');
                         break;
                     case 'terminated': 
-                        console.log('subscription terminated');
+                        console.log('Subscription Terminated');
                         break;
                 }
             });
             await opcuaClient.subscribe([nodeId, nodeId1, nodeId2, nodeId3]);
             //console.log(await opcuaClient.readValue(nodeId3));
-            //const opcuaClient: OPCClientBasic = new OPCClientBasic(endpointUrl, namespace);
-            //opcuaClient.readTag(nodeId);
             //opcuaClient.writeTag(nodeId, 10011, DataType.Int32);
             //opcuaClient.readTag(nodeId);
             // opcuaClient.writeTag(nodeId2, 10011, DataType.Float);
-            // await opcuaClient.ready;
-            // console.log('client ready');
             //await opcuaClient.browseWithClient();
-            //const subscription = await opcuaClient.createSubscription();
-            //console.log('subscription created!');
-            // subscription.on('started', () =>
-            //     console.log('subscription started - subscriptionId=',subscription.subscriptionId)
-            // )
-            // .on('keepalive', () => console.log('keepalive'))
-            // .on('terminated', () => console.log('subscription terminated'));
             //await opcuaClient.browseAllNodes();
             //console.log('browse complete');
-            //await opcuaClient.createSubscription();
-            //await opcuaClient.writeTag();
             resolve();
         } catch (error) {
             reject(error);
