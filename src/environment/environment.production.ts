683d7330a1c99dc6f6196bad3714449bbe3c2fe5
Johnathan Sanders <josanders@microsoft.com>
2024-12-18 17:30:24 -0600

    update environment files

--- src/environment/environment.production.ts
+++ src/environment/environment.production.ts
@@ -1,4 +1,7 @@
 export const Environment = {
+    mqttBroker: {
+        uri: process.env['MQTT_BROKER__URI']
+    },
     opcuaServer: {
         endpointUri: process.env['OPCUA_SERVER__ENDPOINT_URI'],
         namespace: process.env['OPCUA_SERVER__NAMESPACE']
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,7 @@
 export const Environment = {
+    mqttBroker: {
+        uri: 'mqtt://localhost:1883'
+    },
     opcuaServer: {
         endpointUri: 'opc.tcp://192.168.50.15:59100',
         namespace: 'ChevronLD'
