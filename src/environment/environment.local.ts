f7056213febe58c3b275a0c5f030996848ae1ed2
Johnathan Sanders <josanders@microsoft.com>
2024-12-19 11:24:08 -0600

    change for kepware

--- src/environment/environment.local.ts
+++ src/environment/environment.local.ts
@@ -3,7 +3,8 @@ export const Environment = {
         uri: 'mqtt://localhost:1883'
     },
     opcuaServer: {
-        endpointUri: 'opc.tcp://192.168.50.15:59100',
+        endpointUri: 'opc.tcp://192.168.50.15:49320',
+        //endpointUri: 'opc.tcp://192.168.50.15:59100',
         namespace: 'ChevronLD'
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const nodeId: string = 's=ChevronLD.CommDrivers.RAEtherNet_IPDriver1.RAEtherNet_
 const nodeId1: string = 's=ChevronLD.CommDrivers.RAEtherNet_IPDriver1.RAEtherNet_IPStation1.Tags.Controller Tags.aLeakDetectedZone1';
 const nodeId3: string = 's=ChevronLD.CommDrivers.RAEtherNet_IPDriver1.RAEtherNet_IPStation1.Tags.Controller Tags.BS01_Bypass_SolVlv_Output';
 const nodeId2: string = 's=ChevronLD.Model.Variable1';
-
+const nodeId4: string = 's=ChevronLD.L320ER.aiokepwaretest';
 const main = async (): Promise<void> => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -39,7 +39,8 @@ const main = async (): Promise<void> => {
                         break;
                 }
             });
-            await opcuaClient.subscribe([nodeId, nodeId1, nodeId2, nodeId3]);
+            //await opcuaClient.subscribe([nodeId, nodeId1, nodeId2, nodeId3]);
+            await opcuaClient.subscribe([nodeId4]);
             console.log('NAMESPACES ARE==>', await opcuaClient.getNamespaces());
             //console.log(await opcuaClient.readValue(nodeId3));
             //opcuaClient.writeTag(nodeId, 10011, DataType.Int32);
